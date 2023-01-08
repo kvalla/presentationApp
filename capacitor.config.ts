1d9ef452aeb5d848e2babbaa6db0b51c57f9a450
kvalla <vallakarel@gmail.com>
2023-01-08 12:26:44 +0100

    Init projects

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cz.utb.presentationApp',
+  appName: 'PresentationApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
